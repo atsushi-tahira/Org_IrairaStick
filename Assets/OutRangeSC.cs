0dbde6b94785410614f73654f127e0c375ea3e24
atsushitahira <a.tahira3@icloud.com>
2019-07-21 15:20:16 +0900

    システム追加
    ミスの範囲にZ軸を追加、プロトステージを作成

---
+++ Assets/OutRangeSC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutRangeSC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "irairabo"){
+            irairaboSC.playing = false;
+            other.gameObject.GetComponent<Renderer>().material.color = Color.gray;
+            other.gameObject.GetComponent<AudioSource>().mute = true;
+        }
+    }
+}
